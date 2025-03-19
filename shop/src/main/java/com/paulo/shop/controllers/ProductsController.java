2d3630419583994eec5892acdf5b17ada3b4ff34
Pauloand05 <pauloandre050619@gmail.com>
2025-03-19 02:09:11 -0300

    feat: Implementãção de adicionar um novo produto(create)

--- shop/src/main/java/com/paulo/shop/controllers/ProductsController.java
+++ shop/src/main/java/com/paulo/shop/controllers/ProductsController.java
@@ -1,14 +1,25 @@
 package com.paulo.shop.controllers;
 
 import com.paulo.shop.models.Product;
+import com.paulo.shop.models.ProductDto;
 import com.paulo.shop.services.ProductsRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.*;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -23,5 +34,61 @@ public class ProductsController {
         List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products",products);
         return "products/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model){
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto",productDto);
+        return "products/CreateProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+        @Valid @ModelAttribute ProductDto productDto,
+        BindingResult result) {
+
+        if(productDto.getImageFile().isEmpty()){
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+
+        if(result.hasErrors()) {
+            result.getAllErrors().forEach(error -> System.out.println(error.getDefaultMessage()));
+            return "products/CreateProduct";
+        }
+
+        //save image file
+        MultipartFile image = productDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFilename = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images-SpringProject/";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if(!Files.exists( uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFilename),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createdAt);
+        product.setImageFileName(storageFilename);
+
+        repo.save(product);
+
+        return "redirect:/products";
     }
 }
